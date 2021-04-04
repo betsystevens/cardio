d8bc30f6c5be2815bdd4401203cc2a74ffb19e99
betsystevens <betsystevens5@gmail.com>
2021-04-04 11:08:26 -0500

    read numbers from stdin convert to matrix

---
+++ node/readMatrix.js
@@ -0,0 +1,22 @@
+// read comma separated numbers from stdin
+// convert to matrix of numbers
+// usage: node readMatrix.js < someDataFile
+// or
+// input comma separated numbers from command line
+// usage: node readMatrix.js
+// ctrl-d to end input
+
+// process.stdin.resume();
+process.stdin.setEncoding('ascii');
+var inputData = '';
+process.stdin.on('data', (chunk) => {
+  inputData += chunk;
+});
+process.stdin.on('end', function() {
+  // filter out empty last line
+  const lines = inputData.split('\n').filter((line) => line.length > 0);
+  var matrix = lines.map((line) => {
+    return line.split(',').map((string) => parseInt(string, 10));
+  });
+  console.log(matrix);
+});
