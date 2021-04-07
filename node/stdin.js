027fd75b5449b77eaeebba050b1597f3a5520716
betsystevens <betsystevens5@gmail.com>
2021-04-07 11:07:32 -0500

    clear file leave instructions for cardio

--- node/stdin.js
+++ node/stdin.js
@@ -1,22 +1,6 @@
 // read from stdin
 // usage: node stdin.js < someFileToReadFrom
-
-process.stdin.resume();
-process.stdin.setEncoding('ascii');
-var inputData = '';
-process.stdin.on('data', function(chunk) {
-  inputData += chunk;
-});
-process.stdin.on('end', function() {
-  const output = writeOutput(inputData);
-  console.log(output);
-});
-
-function writeOutput(inputData) {
-  // Start writing code here to consume input, and return result.
-
-  // do something amazing
-  let inputArray = inputData.split('\n');
-  // return something
-  return inputArray[0];
-}
+// 1. read file from stdin into a string
+// 2. on end of file call a function, pass string as argument
+//    main(data)    // data it file read
+// 3. main does something amazing and returns a result
