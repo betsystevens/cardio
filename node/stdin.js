7e34b1770835bf5b746233808a0c0c3907d13c9a
betsystevens <betsystevens5@gmail.com>
2021-04-07 11:32:51 -0500

    cardio done

--- node/stdin.js
+++ node/stdin.js
@@ -5,6 +5,10 @@
 //    main(data)    // data it file read
 // 3. main does something amazing and returns a result
 
+// process.stdin is a stream
+// all streams are instances of EventEmitter class
+
+process.stdin.setEncoding('UTF-8');
 var inputData = '';
 process.stdin.on('data', (data) => {
   inputData += data;
