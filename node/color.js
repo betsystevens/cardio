84bb4d399825ad30680f1d83a72d8a155ec4844a
betsystevens <betsystevens5@gmail.com>
2021-04-11 22:04:05 -0500

    log high intensity colors, not much different than original 8

--- node/color.js
+++ node/color.js
@@ -6,15 +6,17 @@ const log = console.log;
  */
 const colorize = () => {
   let reset = '\u001b[39m';
+  let ESC = '\u001b[';
   return {
-    black: (text) => '\u001b[30m' + text + reset,
-    red: (text) => '\u001b[31m' + text + reset,
-    green: (text) => '\u001b[32m' + text + reset,
-    yellow: (text) => '\u001b[33m' + text + reset,
-    blue: (text) => '\u001b[34m' + text + reset,
-    magenta: (text) => '\u001b[35m' + text + reset,
-    cyan: (text) => '\u001b[36m' + text + reset,
-    white: (text) => '\u001b[37m' + text + reset,
+    black: (text) => `${ESC}30m` + text + reset,
+    red: (text) => `${ESC}31m` + text + reset,
+    green: (text) => `${ESC}32m` + text + reset,
+    yellow: (text) => `${ESC}33m` + text + reset,
+    blue: (text) => `${ESC}34m` + text + reset,
+    magenta: (text) => `${ESC}35m` + text + reset,
+    cyan: (text) => `${ESC}36m` + text + reset,
+    white: (text) => `${ESC}37m` + text + reset,
+    set: (code, text) => `${ESC}${code}m` + text + reset,
   };
 };
 
@@ -29,10 +31,30 @@ log(color.magenta(text));
 log(color.cyan(text));
 log(color.white(text));
 
-log(
-  color.red('red fish') +
-    ' ' +
-    color.cyan('cyan fish') +
-    ' ' +
-    color.magenta('magenta fish')
-);
+log();
+let words = [
+  'one',
+  'fish',
+  'two',
+  'fish',
+  'red',
+  'fish',
+  'blue',
+  'fish',
+  'black',
+  'fish',
+  'blue',
+  'fish',
+  'old',
+  'fish',
+  'new',
+  'fish',
+];
+
+// use high intensity colors 90-97
+for (let i = 0; i < 8; i++) {
+  let j = 2 * i;
+  log(
+    color.set(30 + i, words[2 * i]) + ' ' + color.set(90 + i, words[2 * i + 1])
+  );
+}
